d90190992a68a996c464fb4a39caf75836a501fd
Elise Djn <elisedjn@protonmail.com>
2020-07-17 16:19:06 +0200

    splashscreen and winscreen building

---
+++ main.js
@@ -0,0 +1,74 @@
+let main = document.querySelector("main");
+
+const buildSplashScreen = () => {
+  let splashScreen = document.createElement("div");
+  splashScreen.id = "splash-screen";
+  splashScreen.innerHTML = `
+    <h1>SOKOBAN 92</h1>
+      <p>
+        Help Soko-boy to store his boxes on the yellow balls with as few moves
+        as possible. <br />Be careful not to be stucked or you will have to try
+        again!<br /><br />
+        Use your keyboard’s arrows to move.<br />
+        You can only push one box at a time!
+      </p>
+      <img src="img/sokogif.gif" alt="Soko-boy dance" />
+      <button id="start-button">START</button> `;
+  main.appendChild(splashScreen);
+};
+
+const buildGameScreen = () => {
+  // Removing the splash screen
+  main.removeChild(main.firstChild);
+  //creating and adding the canvas to the body
+  let gameScreen = document.createElement("canvas");
+  gameScreen.width = "560";
+  gameScreen.height = "560";
+  gameScreen.id = "game-canvas";
+  main.appendChild(gameScreen);
+};
+
+const buildWinScreen = () => {
+  // Removing the game screen
+  main.removeChild(main.firstChild);
+  // creating and adding the win screen to the body
+  let winScreen = document.createElement("div");
+  winScreen.id = "win-screen";
+  winScreen.innerHTML = `
+    <h1>YEAH!</h1>
+      <h2>YOU MADE IT!</h2>
+      <div><img src="img/sokogif.gif" alt="Soko-boy dance" /></div>
+      <p id="final-score"> Your moves : <span>20</span></p>
+      <button id="play-again-btn">Play again</button>`;
+  main.appendChild(winScreen);
+};
+
+const changeColorBtn = (btn) => {
+  btn.style.color = "#ff0000";
+  btn.style.backgroundColor = "#7f0000";
+};
+
+// Waiting for the page to load before to show the splash screen
+window.addEventListener("load", () => {
+  buildSplashScreen();
+
+  //Waiting for the player to press Start to build the game
+  let startBtn = document.querySelector("#start-button");
+  startBtn.addEventListener("click", () => {
+    changeColorBtn(startBtn);
+    buildGameScreen();
+    // Waiting for the player to win the game
+    // !!!! I Need to create a never ending loop between play again and the game screen
+    // Begining of a logic
+    let win = false;
+    if (win) {
+      buildWinScreen();
+      // Waiting for the player to click on play again
+      let playAgainBtn = document.querySelector("#play-again-btn")
+      playAgainBtn.addEventListener("click", () => {
+        changeColorBtn(playAgainBtn);
+        buildGameScreen();
+      })
+    }
+  });
+});
